49c8743e59b1ad5ab7b7a80379c8d16dfa6aa7ce
swathi-ops <swathipottabathini@gmail.com>
2021-05-04 15:25:29 +0530

    todo

--- components/common.sh
+++ components/common.sh
@@ -7,15 +7,24 @@ Head(){
 }
 
 OS_PREREQ(){
-  set-hostname component/$COMPONENT
+  set-hostname component/${COMPONENT}
   Head " Updating the repos "
   apt update &>>$LOG
   }
+
+STAT(){
+  if [ "${1}" -eq 0 ]; then
+    echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILURE\e[0m"
+    echo  -e "\e[1m;31m Pls refer log file for more information, log filepath is =${LOG}\e[0m"
+  fi
+}
+
 ERROR(){
   echo -e "\e[1;31m$1\e[0m"
 }
-DOWNLOAD_COMPONENT()
-{
+DOWNLOAD_COMPONENT(){
   Head "Downloading ${COMPONENT} component"
 git clone "https://github.com/zelar-soft-todoapp/${COMPONENT}.git"
 }
--- components/frontend.sh
+++ components/frontend.sh
@@ -3,7 +3,7 @@
 source components/common.sh
 OS_PREREQ
 Head "Installing Nginx"
-apt install nginx -y &>>$LOG
+apt install nginx -y &>>"$LOG"
 systemctl start nginx
 Head "Installing npm"
 apt install npm -y &>>$LOG
@@ -13,5 +13,28 @@ Head "Downloading git file"
 cd /var/www/html
 DOWNLOAD_COMPONENT
 cd ${COMPONENT}
+HEAD "Install Npm"
+npm install >>"${LOG}"
+STAT $?
+HEAD "Run build"
+npm run build >>"${LOG}"
+killall node >>"${LOG}"
+npm run build
+STAT $?
+HEAD "Change root path in nginx"
+cd /etc/nginx/sites-available || exit
+vi default
+STAT $?
+HEAD "Update index.js File With Todo & Login Ip"
+cd /var/www/html/todo/frontend && cd config || exit
+vi index.js
+STAT $?
+HEAD "Restart Nginx"
+systemctl restart nginx
+STAT $?
+HEAD "run npm start"
+npm start
+STAT $?
+
 npm install
 npm run build
