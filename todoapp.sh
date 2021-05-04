98427168e0551c4c487a75bb03a9ee6ffc4f1e08
swathi-ops <swathipottabathini@gmail.com>
2021-05-04 07:39:16 +0530

    todo

--- todoapp.sh
+++ todoapp.sh
@@ -3,15 +3,17 @@
 COMPONENT=$1
 source components/common.sh
 
-if [! -f components/${COMPONENT}.sh ]; then
-  echo -e "\e[31mInvalid component name\e[0m"
+if [ ! -f components/${COMPONENT}.sh ]; then
+  ERROR "Invalid Component Name is Provided"
   exit 1
-  fi
-  USER_NAME =$(whoami)
-  if ($"{USER_NAME}" != "root");then
-  echo "\e[32mYou should be in root to perform this script\e[0m"
-  exit
-  fi
+fi
 
-  export COMPONENT
-  bash components/${COMPONENT}.sh
+USER_NAME=$(whoami)
+
+if [ "${USER_NAME}" != "root" ]; then
+  ERROR "You should be a root user to perform this script, Try with sudo"
+  exit 1
+fi
+
+export COMPONENT
+bash components/${COMPONENT}.sh
