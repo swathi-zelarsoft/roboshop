e7c10e71714c3e65a0846b9a054bfc5171b17658
swathi-ops <swathipottabathini@gmail.com>
2021-05-04 10:46:03 +0530

    todo

--- components/common.sh
+++ components/common.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+Head(){
+  echo -e "\e[36m  ============ $1 ===========\e[0m"
+}
+LOG=/tmp/${COMPONENT}.log
+rm -r $LOG
 OS_PREREQ(){
   set-hostname component/$COMPONENT
-  apt update
+  Head " Updating the repos"
+  apt update &>$LOG
   }
 ERROR(){
   echo -e "\e[1;31m$1\e[0m"
