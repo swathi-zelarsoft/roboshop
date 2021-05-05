10840175f86fd2b8675e64f3ebbb98f2f87cfb1b
swathi-ops <swathipottabathini@gmail.com>
2021-05-05 15:58:34 +0530

    initial commits

--- components/redis.sh
+++ components/redis.sh
@@ -4,7 +4,7 @@ source components/common.sh
 OS_PREREQ
 
 Head "Install redis server"
-sudo apt install redis-server >>"${LOG}"
+sudo apt install redis-server -y >>"${LOG}"
 
 Head "Change ip address in redis config file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
