59022c5b1c120343c57e6e2b698c25fb4c559935
swathi-ops <swathipottabathini@gmail.com>
2021-05-05 15:21:35 +0530

    initial commits

--- components/users.sh
+++ components/users.sh
@@ -4,9 +4,10 @@ source components/common.sh
 
 Head "Set hostname and update repo"
 OS_PREREQ
-
+Head "Remove Java-11 version"
+apt-get remove openjdk-11-jdk-headless -y &>>$LOG
 Head "install java 8 version"
-apt-get install openjdk-8-jdk -y &>>"${LOG}"
+apt-get install openjdk-8-jdk -y &>>${LOG}
 STAT $?
 Head "check java version"
 java -version
