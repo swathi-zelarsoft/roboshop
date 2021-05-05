38ef5f10cc81f177b63b89654882f09123b7b75a
swathi-ops <swathipottabathini@gmail.com>
2021-05-05 15:51:54 +0530

    initial commits

--- components/todo.sh
+++ components/todo.sh
@@ -2,13 +2,14 @@
 
 source components/common.sh
 OS_PREREQ
+
 Head "Installing npm"
 apt install npm -y &>>$LOG
 
 DOWNLOAD_COMPONENT
 
 Head "Install npm"
-npm install
+npm install &>>$LOG
 STAT $?
 
 Head  "Create service file"
