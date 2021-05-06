50665d89bc036be259c9b2f0a21d33d3ab465fdf
swathi-ops <swathipottabathini@gmail.com>
2021-05-06 17:31:06 +0530

    initial commits

--- components/login.sh
+++ components/login.sh
@@ -29,11 +29,11 @@ Head "Build"
 go build &>>"${LOG}"
 STAT $?
 Head "Update USERS_API in Service File"
-sed -i -e "s/USERSDNS/users.zsdevtraining.online/" /go/src/login/systemd.service
+sed -i -e "s/USERSDNS/users.zsdevtraining.online/" /root/go/src/login/systemd.service
 Stat $?
 
 Head "Create login service file"
-mv /go/src/login/systemd.service /etc/systemd/system/login.service
+mv /root/go/src/login/systemd.service /etc/systemd/system/login.service
 
 
 Head "Start login service"
