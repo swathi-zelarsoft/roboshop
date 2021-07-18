0688d39294e026361e12845dd6f85f8c39bde302
swathi-ops <swathipottabathini@gmail.com>
2021-07-18 08:06:51 +0530

    deployment

--- instance-creation/create.sh
+++ instance-creation/create.sh
@@ -25,8 +25,6 @@ Instance_Create() {
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z06752241NSSN6BC0B4NW --change-batch file:///tmp/record.json
-  sed -i -e "/${COMPONENT}/ d" ../inv
-  echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inv
 }
 
 if [ "$1" == "all" ]; then
