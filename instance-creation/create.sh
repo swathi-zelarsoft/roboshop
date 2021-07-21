25485a7b19acf226252ccf7859d0290d1a279abe
swathi-ops <swathipottabathini@gmail.com>
2021-07-21 06:45:03 +0530

    deployment

--- instance-creation/create.sh
+++ instance-creation/create.sh
@@ -24,7 +24,7 @@ Instance_Create() {
   IPADDRESS=$(aws ec2 describe-instances  --filters Name=tag:Name,Values=${COMPONENT} | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z06752241NSSN6BC0B4NW --change-batch file:///tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z03139592HYDUWQ5MHUK4 --change-batch file:///tmp/record.json
 }
 
 if [ "$1" == "all" ]; then
